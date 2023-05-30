1a69ebef88cc52f2b276ae65f9ff9ed8d7f77f97
krismarie98 <kris91398@gmail.com>
2023-05-30 16:50:36 -0400

    adding new queries

--- diabetes dataset/diabetes_queries.sql
+++ diabetes dataset/diabetes_queries.sql
@@ -14,18 +14,16 @@ diabetes int not null
 select*
 from diabetes;
 
--- bring up the new data base
-select *
-from diabetes;
-
 -- Check the different tables
 select gender
 from diabetes;
 
 -- Find the average age of the study
 select avg(age)
-from diabetes;
--- results are 41.88
+from diabetes
+where diabetes = 1;
+-- results are 60.94
+-- Visualization created via Tableau
 
 -- Check for null values
 select gender, age, hypertension, heart_disease, hba1c_level, smoking_history, bmi, blood_glucose_level, diabetes
@@ -46,28 +44,61 @@ select count(diabetes)
 from diabetes 
 where diabetes=1;
 -- Results are 8500
+-- Visualization created.
 
 -- Find the coun of people positive for hypertension
 select count(hypertension)
 from diabetes 
 where hypertension=1;
 -- Results are 7485
+-- Visualization created
 
 -- Find the count of people positive for heart disease
 select count(heart_disease)
 from diabetes 
 where heart_disease=1;
 -- Results are 3942
+-- Visualization created
 
 -- The average blood glucose level
 select avg(blood_glucose_level)
 from diabetes;
 -- Resulta are 138.05
+-- Visualization created
+
+-- Find the average blood glucode level of people who have diabetes 
+select avg(blood_glucose_level) 
+from diabetes 
+where diabetes =1;
+-- Results for avg blood glucose level for people with diabetes are 194.09
+-- Visualization created
+
+--and people who do not have diabetes
+select avg(blood_glucose_level) 
+from diabetes 
+where diabetes =0;
+-- Results for avg blood glucose level for people without diabetes are 132.85
+-- Visualizated created
 
 -- The average hba1c level
 select avg(hba1c_level)
 from diabetes;
 --Results are 5.52
+-- Visualizated created
+
+-- Find the average hbA1c level of people who have diabetes 
+select avg(hba1c_level) 
+from diabetes 
+where diabetes =1;
+-- Results are 6.93
+-- Visualizated created
+
+-- and avg hbA1c levels of people who dont have diabetes
+select avg(hba1c_level) 
+from diabetes 
+where diabetes =0;
+-- Results are 5.39
+-- Visualizated created
 
 -- Find the unique count for smoking history
 -- There are six answers for smoing history : never, current, ever, former, never, no info, not current
@@ -88,6 +119,18 @@ select avg(bmi)
 from diabetes;
 -- Results are 27.32
 
+-- Find the average bmi for people with diabetes
+select avg(bmi)
+from diabetes
+where diabetes =1;
+--Results are 31.98
+
+-- Find the average bmi for people without diabetes
+select avg(bmi)
+from diabetes
+where diabetes =0;
+-- Results are 26.88
+
 -- Find the min bmi
 select min(bmi)
 from diabetes;
@@ -97,4 +140,12 @@ from diabetes;
 select max(bmi)
 from diabetes;
 -- Results are 95.69
+
+-- Query to find the count of gender with diabetes
+select gender,count(gender) 
+from diabetes
+group by gender;
+-- Visualization created via Tableau
+
+
 
